b2eb80f874dbc043d8b43bbd8fef65ce30ef74b4
Ernest Bruno <erndobruno@gmail.com>
2021-12-09 14:19:16 -0800

    Create a schema for a flight with validations.

---
+++ models/flight.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const flightSchema = new mongoose.Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    },
+    flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        default: new Date().getFullYear() 
+    }
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
