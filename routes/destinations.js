965d741bc6e6423b51dc03b518ce68ac02ae3915
Ernest Bruno <erndobruno@gmail.com>
2021-12-09 17:28:36 -0800

    Add a routes file for destinations.

---
+++ routes/destinations.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+var flightsCtrl = require('./../controllers/flights');
+
+/* POST /flights/:id/destinations */
+
+
+module.exports = router;
