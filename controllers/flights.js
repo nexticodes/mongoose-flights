f30692e5e38b37a9e9d10eb8451bc3e972b1ab33
Ernest Bruno <erndobruno@gmail.com>
2021-12-09 16:21:42 -0800

    Add a controller action that creates the body based on content of request body.

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,14 +1,25 @@
 module.exports = {
     index,
     new: newForm,
-}
+    create,
+};
 
 const Flight = require('../models/flight');
 
 function index(req, res){
-    res.render('flights/index', { title: 'All Flights'});
-}
+    Flight.find({}, function(err, flights){
+        if (err) console.log(err);
+        res.render('flights/index', { title: 'All Flights', flights});
+    });
+};
 
 function newForm(req, res){
-    res.render('flights/new', {title: 'Add Flight'})
-}
+    res.render('flights/new', {title: 'Add Flight'});
+};
+
+function create(req, res){
+    Flight.create(req.body, function(err, flight){
+        if (err) console.log(err);
+        res.redirect('/flights');
+    });
+};
