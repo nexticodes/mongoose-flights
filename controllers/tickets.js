3b48375ef22877a12147755f1ec21c00b82fd07a
Ernest Bruno <erndobruno@gmail.com>
2021-12-10 15:59:58 -0800

    Make a controller action for creating a ticket.

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,7 +1,17 @@
 module.exports = {
     new: newTicket,
-}
+    create,
+};
+
+const Ticket = require('./../models/ticket');
 
 function newTicket(req, res){
-    res.render('tickets/new', { title: `Add Ticket`, flightId: req.params.id })
-}
+    res.render('tickets/new', { title: `Add Ticket`, flightNo: req.params.id })
+};
+
+function create(req, res){
+    req.body.flight = req.params.id;
+    Ticket.create(req.body, function(err){
+        res.redirect(`/flights/${req.params.id}`);
+    });
+};
