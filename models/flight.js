490eb4386b2c00737acd95da26d82f9b4c9496ac
Ernest Bruno <erndobruno@gmail.com>
2021-12-09 16:51:41 -0800

    Adds destination schema into model and as a subdocument for flight schema.

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    },
+    arrival: {
+        type: Date
+    }
+},{
+    timestamps: true
+});
+
 const flightSchema = new Schema({
     airline: {
         type: String,
@@ -21,7 +33,8 @@ const flightSchema = new Schema({
         default: function() {
             return new Date().getFullYear() + 1
         }
-    }
+    },
+    destinations: [destinationSchema]
 }, {
     timestamps: true
 });
