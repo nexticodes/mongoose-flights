7973aaa89086e1b943c3a0b63e7c4b6f3ace3c0e
Ernest Bruno <erndobruno@gmail.com>
2021-12-10 16:46:46 -0800

    Create a removeTicket action that handles deleting one ticket when user clicks.

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,6 +1,7 @@
 module.exports = {
     new: newTicket,
     create,
+    delete: removeTicket
 };
 
 const Ticket = require('./../models/ticket');
@@ -13,5 +14,11 @@ function create(req, res){
     req.body.flight = req.params.id;
     Ticket.create(req.body, function(err){
         res.redirect(`/flights/${req.params.id}`);
+    });
+};
+
+function removeTicket(req, res){
+    Ticket.deleteOne({_id: req.params.id }, function(err){
+        res.redirect(`/flights/${req.body.flightId}`)
     });
 };
