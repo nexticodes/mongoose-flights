f165b869501960e57c448a2350a8c1dd4272e520
Ernest Bruno <erndobruno@gmail.com>
2021-12-10 16:46:15 -0800

    Add a delete route.

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 const ticketsCtrl = require('./../controllers/tickets');
 
+/* POST / DELETE tickets/:id */
+router.delete('/tickets/:id', ticketsCtrl.delete);
+
 /* GET /flights/:id/tickets/new for new form */
 router.get('/flights/:id/tickets/new', ticketsCtrl.new);
 
 /* POST /flights/:id/tickets */
 router.post('/flights/:id/tickets', ticketsCtrl.create);
+
 
 module.exports = router;
